34bc9827903dbcc8357cf3458c851990bf8b96e1
github username <github email address>
2018-03-07 10:01:10 -0500

    done

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,32 +1,45 @@
-import React, { Component } from 'react'
-import TransactionsList from './TransactionsList'
-import Search from './Search'
-import {transactions} from '../transactionsData'
+import React, { Component } from "react";
+import TransactionsList from "./TransactionsList";
+import Search from "./Search";
+import { transactions } from "../transactionsData";
 
 class AccountContainer extends Component {
+  state = {
+    transactions: [],
+    filter: ""
+  };
+  // get a default state working with the data imported from TransactionsData
+  // use this to get the functionality working
+  // then replace the default transactions with a call to the API
 
-  constructor() {
-    super()
-
-    // get a default state working with the data imported from TransactionsData
-    // use this to get the functionality working
-    // then replace the default transactions with a call to the API
-
+  componentDidMount() {
+    fetch("https://boiling-brook-94902.herokuapp.com/transactions")
+      .then(res => res.json())
+      .then(json =>
+        this.setState({
+          transactions: json
+        })
+      );
   }
 
-  handleChange(event) {
-    // your code here
-  }
+  handleChange = event => {
+    this.setState({
+      filter: [event.target.value.toLowerCase()]
+    });
+  };
 
   render() {
-
+    console.log(this.state.filter);
     return (
       <div>
-        <Search />
-        <TransactionsList />
+        <Search handleChange={this.handleChange} />
+        <TransactionsList
+          transactions={this.state.transactions}
+          filter={this.state.filter}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default AccountContainer
+export default AccountContainer;
